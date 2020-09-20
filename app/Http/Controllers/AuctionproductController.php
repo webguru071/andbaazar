0ad1e4360f61e3680035800b3012153c5f912da4
Rupa Paik <rupaik.cse@gmail.com>
2020-09-20 15:41:39 +0600

    Auction index page design

--- app/Http/Controllers/AuctionproductController.php
+++ app/Http/Controllers/AuctionproductController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use App\Auctionproduct;
+
 use Illuminate\Http\Request;
+use App\Mail\ProductApproveRequestMail;
+use App\Mail\productApproveMail;
+use App\Mail\ProductRejectMail;
+use App\Models\RejectValue;
 use App\Models\Category;
+use App\Models\Auctionproduct;
 use Sentinel;
 use Session;
 use Baazar;
@@ -16,9 +21,60 @@ class AuctionproductController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        return view('auction.product.index');
+         // $cats = \DB::table('products')->select('category_id')->distinct()->get();
+      // dd($cats);
+      $product = Auctionproduct::with('user')->where('shop_id',Baazar::shop()->id);
+     
+
+      $cat = $product->select('category_id')->distinct()->get();
+      // dd($cat);
+      // $sellerProfile = Merchant::with('rejectvalue')->where('user_id',Sentinel::getUser()->id)->first();
+      $item = Auctionproduct::where('user_id',Sentinel::getUser()->id)->get();
+      // dd($item);
+      $product = Auctionproduct::with('rejectvalue')->where('shop_id',Baazar::shop()->id)->where('type','ecommerce');
+      // dd($product);
+      $rejectReason = RejectValue::where('user_id',Sentinel::getUser()->id)->where('type','ecommerce')->get();
+      // dd($rejectReason);
+      // $items = Product::with('inventory')->paginate('10');
+      $filter = [
+        'category'  => '',
+        'status'  => '',
+        'keyword'  => '',
+      ];
+      $findCat = Auctionproduct::where('shop_id',Baazar::shop()->id)->where('type','ecommerce');
+          $categories = $findCat->select('category_id')->with('category')->distinct()->get();
+
+      if ($request->has('cat')){
+        $product = Auctionproduct::where('shop_id',Baazar::shop()->id)->where('category_slug','like','%'.$request->cat.'%')->where('type','ecommerce');
+        }
+      //Category Filter
+      if ($request->has('category') && !empty($request->category)){
+        $catId = Category::where('slug',$request->category)->first();
+        if($catId){
+          $product = $product->where('category_id',$catId->id);
+        }
+        $filter['category'] = $request->category;
+
+      }
+      
+      //status Filter
+      if ($request->has('status') && !empty($request->status)){
+        $product = $product->where('status',$request->status);
+        $filter['status'] = $request->status;
+      }
+
+      //status Filter
+      if ($request->has('keyword') && !empty($request->keyword)){
+        $product = $product->where('name','like','%'.$request->keyword.'%');
+        $filter['keyword'] = $request->keyword;
+      }
+
+      // dd($product);
+      $product = $product->paginate(10);
+      $product = $product->withPath("products?keyword={$filter['keyword']}&category={$filter['category']}&status={$filter['status']}");
+      return view ('auction.product.index',compact('product','categories','filter'));       
     }
 
     /**
