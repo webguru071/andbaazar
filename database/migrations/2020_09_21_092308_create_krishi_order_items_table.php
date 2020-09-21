be49917390d74e6889ace7bf0d81d54f29ed70da
Rupa Paik <rupaik.cse@gmail.com>
2020-09-21 16:51:41 +0600

    migrate krishi product and krishi order table

--- database/migrations/2020_09_21_092308_create_krishi_order_items_table.php
+++ database/migrations/2020_09_21_092308_create_krishi_order_items_table.php
@@ -14,7 +14,25 @@ class CreateKrishiOrderItemsTable extends Migration
     public function up()
     {
         Schema::create('krishi_order_items', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');                     
+            $table->integer('qty');
+            $table->string('unit')->nullable();
+            $table->string('rate')->nullable();
+            $table->text('amount');
+            $table->boolean('active')->default(1)->change();
+            $table->string('available_from');
+            $table->string('frequency_support');
+   
+            $table->softDeletes();
+            $table->unsignedBigInteger('user_id');                  
+            $table->unsignedBigInteger('order_id'); 
+            $table->unsignedBigInteger('shop_id');  
+            $table->unsignedBigInteger('krishi_product_id');    
+        
+            $table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('shop_id')->references('id')->on('shops')->onDelete('cascade')->onUpdate('cascade'); 
+            $table->foreign('krishi_product_id')->references('id')->on('krishi_products')->onDelete('cascade')->onUpdate('cascade');           
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
             $table->timestamps();
         });
     }
