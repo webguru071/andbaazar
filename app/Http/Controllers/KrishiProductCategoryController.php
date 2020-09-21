4590e221480d75d2659c5378c0f33396029e759a
Rupa Paik <rupaik.cse@gmail.com>
2020-09-21 15:57:59 +0600

    Added Krishibaazar model contoller and table

---
+++ app/Http/Controllers/KrishiProductCategoryController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\KrishiProductCategory;
+use Illuminate\Http\Request;
+
+class KrishiProductCategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\KrishiProductCategory  $krishiProductCategory
+     * @return \Illuminate\Http\Response
+     */
+    public function show(KrishiProductCategory $krishiProductCategory)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\KrishiProductCategory  $krishiProductCategory
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(KrishiProductCategory $krishiProductCategory)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\KrishiProductCategory  $krishiProductCategory
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, KrishiProductCategory $krishiProductCategory)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\KrishiProductCategory  $krishiProductCategory
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(KrishiProductCategory $krishiProductCategory)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/KrishiProductController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\KrishiProduct;
+use Illuminate\Http\Request;
+
+class KrishiProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\KrishiProduct  $krishiProduct
+     * @return \Illuminate\Http\Response
+     */
+    public function show(KrishiProduct $krishiProduct)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\KrishiProduct  $krishiProduct
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(KrishiProduct $krishiProduct)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\KrishiProduct  $krishiProduct
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, KrishiProduct $krishiProduct)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\KrishiProduct  $krishiProduct
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(KrishiProduct $krishiProduct)
+    {
+        //
+    }
+}
