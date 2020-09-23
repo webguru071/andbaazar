61fac1e0f904f9e1cb39c91bf6160a149d1bae44
Rupa Paik <rupaik.cse@gmail.com>
2020-09-23 13:52:19 +0600

    Solve problem on padding in selectbox

--- resources/views/merchant/product/krishibaazar/create.blade.php
+++ resources/views/merchant/product/krishibaazar/create.blade.php
@@ -22,11 +22,12 @@
   border-radius: 0 !important;
   border: solid 1px #c4c4c4 !important;
   padding-left: 4px;
+  padding-top:7px;
 }
 
 .select2-selection--multiple {
   height: 70px !important;
-  width: 800px !important;
+  width: 975px !important;
   overflow: hidden;
 }
 
@@ -155,7 +156,7 @@
 
        /* Iamge */
        .h-100{
-            height: 10px !important;
+            height: 100px !important;
             margin: 4px;
         }
         .drop-area{
@@ -301,51 +302,7 @@
                                     @endif
 
                                 </div> 
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label for="frequency">Frequency</label>
-                                        {{-- <select class="form-control  js-example-basic-multiple" name="frequency[]" multiple="multiple" required>
-                                            <option value="">Select product frequency</option> 
-                                            <option value="sunday">Sunday</option>
-                                            <option value="monday">Monday</option>
-                                            <option value="tuesday">Tuesday</option>
-                                            <option value="wednessday">Wednessday</option>
-                                            <option value="thursday">Thursday</option>
-                                            <option value="friday">Friday</option>
-                                            <option value="saturday">Saturday</option>
-                                            <option value="everyday">Everyday</option>
-                                            <option value="weekly">Weekly</option>
-                                            <option value="fortnightly">Fortnightly</option>
-                                            <option value="monthly">Monthly</option>
-                                        </select> --}}
-                                        {{-- label for="subject">Subject</label> --}}
-  		{{-- <label>:</label> --}} 
-                                    </div>
-                                </div>   
-                                {{-- <div class="row">
-                                    <div class="col-md-6">
-                                        <div class="form-group margin">
-                                            <label for="qty">Quantity<span>*</span></label>
-                                            <input type="number" class="form-control" name="qty" id="qty" />
-                                            <span class="text-danger" id="message_qty"></span>
-                                            @if ($errors->has('qty'))
-                                            <span class="text-danger">{{ $errors->first('qty') }}</span>
-                                            @endif
-                                        </div>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <div class="form-group margin">
-                                            <label for="unit">Unit<span>*</span></label>
-                                            <input type="text" class="form-control" name="unit" id="unit" />
-                                            <span class="text-danger" id="message_unit"></span>
-                                            @if ($errors->has('unit'))
-                                            <span class="text-danger">{{ $errors->first('unit') }}</span>
-                                            @endif
-                                        </div>
-                                    </div>
-                                </div> --}}
-
-                                </div>                                                         
+                                                                                  
 
                                 <div class="form-unit form-divided">
                                     <label for="emp-id" class="form-input-label pr-5">Frequency:</label><br>
