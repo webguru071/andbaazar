a44268ffd959f1802d4b5ef842f010277a6caa64
Shariful Islam <shariful.info56@gmail.com>
2021-01-20 15:33:21 +0600

    clean up code customer,contact,billing,shipping etc.)

--- routes/frontend.php
+++ routes/frontend.php
@@ -1,10 +1,5 @@
 <?php
 
-Route::get('login','CustomerController@userlogin');
-Route::post('login','CustomerController@userloginprocess')->name('userloginprocess');
-Route::get('register','CustomerController@register');
-Route::post('register','CustomerController@registration')->name('registration');
-Route::get('dashboard','CustomerController@dashboard');
 Route::get('logout','AuthController@logout');
 
 // Frontend Routes Are Start Here...............
@@ -21,28 +16,6 @@ Route::resource('contact-us', 'ContactController');
 
 // Route::middleware(['auth'])->prefix('andbaazaradmin')->group(function () {
 Route::prefix('customer')->group(function () {
-    Route::get('/','CustomersController@create');
-    Route::post('/','CustomersController@store')->name('profileUpdate');
-    Route::get('shipping/{slug}/edit','CustomerShippingAddressesController@edit');
-    Route::put('shipping/{slug}','CustomerShippingAddressesController@update');
-    Route::resource('shipping','CustomerShippingAddressesController');
-    Route::get('billing/{slug}/edit','CustomerBillingAddressesController@edit');
-    Route::put('billing/{slug}','CustomerBillingAddressesController@update');
-    Route::resource('billing','CustomerBillingAddressesController');
-    Route::get('card/{slug}/edit','CustomerCardsController@edit');
-    Route::put('card/{slug}','CustomerCardsController@update');
-    Route::resource('card','CustomerCardsController');
     Route::resource('buyerpayment','BuyerPaymentsController');
 });
-
-// Customer Routes Are End Here...............
-// forgot password route....
-
-Route::get('forgot_password', 'CustomerController@forgot');
-Route::post('forgot_password', 'CustomerController@password');
-
-// Reset  Password Route....
-
-Route::get('reset_password/', 'CustomerController@reset');
-Route::put('reset_password/{email}', 'CustomerController@updatePassword');
 
